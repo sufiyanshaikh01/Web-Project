4b3ffafd10c2ba1ae8644957128f88d1e44edc4f
sufiyanshaikh01 <softwaremastar0010@gmail.com>
2026-08-11 21:06:53 +0530

    Project logic is add

---
+++ Tic-Tac-Toe/app.js
@@ -0,0 +1,53 @@
+let box = document.querySelectorAll(".box");
+let reset = document.querySelector("#reset");
+let newGame = document.querySelector("#newGame");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true;
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8]
+   ];
+
+box.forEach((box) => {
+    box.addEventListener('click', () => {
+        console.log("Box is clicked ");
+        if (turnO) {
+            box.innerText = "O";
+            turnO = false;
+        } else {
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+        checkWinner();
+    });    
+});
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulation Winner is: ${winner}`;
+    msgContainer.classList.remove("hide");
+};
+
+const checkWinner = () => {
+    for (let pattern of winPatterns){
+        let pos1val = box[pattern[0]].innerText;
+        let pos2val = box[pattern[1]].innerText;
+        let pos3val = box[pattern[2]].innerText;
+        
+        if (pos1val != "" && pos2val != "" && pos3val != ""){
+            if (pos1val === pos2val && pos2val === pos3val){
+         console.log("Winner", pos1val);
+         showWinner(pos1val);
+            }
+        }    
+    }   
+};
